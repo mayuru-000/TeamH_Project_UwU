35e0b170bb270c51e9c52505a810774e9cef347a
dekura <>
2025-10-14 17:13:53 +0900

    10/14 プログラム更新

---
+++ project/Source/Granade.cpp
@@ -0,0 +1,21 @@
+#include "Granade.h"
+#include <DxLib.h>
+#include <vector>
+
+using namespace std;
+
+Granade::Granade()
+{
+}
+
+Granade::~Granade()
+{
+}
+
+void Granade::Update() {
+
+}
+
+void Granade::Draw() {
+
+}
---
+++ project/Source/Granade.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../Library/SceneBase.h"
+
+class Granade : public SceneBase
+{
+public:
+	Granade();
+	~Granade();
+	void Update() override;
+	void Draw() override;
+
+private:
+
+};
+
